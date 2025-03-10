d7a9f8862b1b5cc5e65fb9c27b66b13102e54c4e
liamreal <liam.kerrin@ucdconnect.ie>
2025-03-10 15:35:52 +0000

    added config file

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.net.*;
+import java.io.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        try {
+            URL myURL = new URL("https://example.com/");
+            URLConnection myURLConnection = myURL.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    myURLConnection.getInputStream()));
+            String inputLine;
+            while ((inputLine = in.readLine()) != null)
+                System.out.println(inputLine);
+            in.close();
+        }
+        catch (MalformedURLException e) {
+            System.out.println("new URL() failed");
+        }
+        catch (IOException e) {
+            System.out.println("openConnection() failed");
+        }
+    }
+}
