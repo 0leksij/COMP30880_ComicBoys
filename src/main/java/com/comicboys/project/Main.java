205093362220bdb38e5a6b3430f72ee7d66455c3
liamreal <liam.kerrin@ucdconnect.ie>
2025-03-13 10:13:22 +0000

    added maven to project, restructured directories

--- src/main/java/com/comicboys/project/Main.java
+++ src/main/java/com/comicboys/project/Main.java
@@ -5,7 +5,6 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-
         try {
             URL myURL = new URL("https://www.scrapethissite.com/pages/");
             URLConnection myURLConnection = myURL.openConnection();
