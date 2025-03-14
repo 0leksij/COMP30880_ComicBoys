28ddce65e8fac30351438c0945e6f5045f3f7e8a
liamreal <liam.kerrin@ucdconnect.ie>
2025-03-14 12:58:41 +0000

    added simple test to see if junit working

---
+++ src/test/java/com/comicboys/config/ConfigurationFileTest.java
@@ -0,0 +1,15 @@
+package com.comicboys.config;
+
+//import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+import org.junit.*;
+import static org.junit.Assert.assertTrue;
+
+public class ConfigurationFileTest {
+
+    @Test
+    public void demoTestMethod() {
+        assertTrue(true);
+    }
+}
