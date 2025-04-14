bfe59973355f9b6755b49ff1271a6dfddb99e1b2
liamreal <liam.kerrin@ucdconnect.ie>
2025-04-14 10:35:45 +0100

    made remove nodes method into their own interfaced

--- src/main/java/com/comicboys/project/utility/XMLFileManager.java
+++ src/main/java/com/comicboys/project/utility/XMLFileManager.java
@@ -18,7 +18,7 @@ import java.io.StringWriter;
 import java.util.List;
 import java.util.Objects;
 
-public interface XMLFileManager{
+public interface XMLFileManager extends XMLNodeRemover {
 
 
     // get directory folder is in
@@ -74,65 +74,16 @@ public interface XMLFileManager{
             return null;
         }
     }
-    // default element is <comic>
     static NodeList selectElements(Document doc) {
         return doc.getElementsByTagName("comic");
     }
-    // specify element to pick
     static NodeList selectElements(Document doc, String element) {
         return doc.getElementsByTagName(element);
     }
-    // method overloading, single string case: pass string as list into same named method with different signature
-    static void removeAllByTag(Node node, String childTagToRemove) {
-        removeAllByTag(node, List.of(childTagToRemove));
-    }
-    // specify which element tags to remove
-    static void removeAllByTag(Node node, List<String> childrenTagsToRemove) {
-        Element element = (Element) node;
-        for (String childTagToRemove : childrenTagsToRemove) {
-            NodeList children = element.getElementsByTagName(childTagToRemove);
-            removeAllChildren(children);
-        }
-    }
-    // remove first child element of a node
-    static void removeFirstChild(Node node) {
-        Element element = (Element) node;
-        NodeList children = element.getChildNodes();
-        removeNChildren(children, 1);
-    }
-    // remove all children elements of a node
-    static void removeAllChildren(NodeList children) {
-        removeNChildren(children, -1); // negative numbers means all children removed
-    }
-    // main remove element method, removes all nodes in list from their respective parent
-    // if numChildrenToRemove < 0, will remove all children
-    private static void removeNChildren(NodeList children, int numChildrenToRemove) {
-        // look through children in ascending order
-        int currentLength = children.getLength(); // list size will be changed throughout
-        int i = 0; // loop variable
-        int childrenRemoved = 0; // children removed so far
-        // since modifying list size, mutable upper bound variable (updated in loop)
-        while (i < currentLength) {
-            Node child = children.item(i);
-            // check is element node
-            if (child.getNodeType() == Node.ELEMENT_NODE) {
-                // if number of children to be removed is reached
-                if (childrenRemoved == numChildrenToRemove) break;
-                // check parent exists
-                if (child.getParentNode() != null) {
-                    // remove child from parent
-                    child.getParentNode().removeChild(child);
-                    // decrement i so does not skip next element, list length also decremented since item removed
-                    i--;
-                    currentLength--;
-                    // increment children removed
-                    childrenRemoved++;
-                }
-            }
-            // next child
-            i++;
-        }
-    }
+    static void removeAllByTag(Node node, String childTagToRemove) { XMLNodeRemover.removeAllByTag(node, childTagToRemove); }
+    static void removeAllByTag(Node node, List<String> childrenTagsToRemove) { XMLNodeRemover.removeAllByTag(node, childrenTagsToRemove); }
+    static void removeFirstChild(Node node) { XMLNodeRemover.removeFirstChild(node); }
+    static void removeAllChildren(NodeList children) { XMLNodeRemover.removeAllChildren(children); }
     // general method to get parent
     static Node getParent(Node node) {
         if (node == null) { return null; }
